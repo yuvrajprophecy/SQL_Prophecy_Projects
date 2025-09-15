413150c5057bad0108757a8b523a1d7e65fec5df
yuvraj@prophecy.io <yuvraj@prophecy.io>
2025-09-15 12:47:15 +0000

    v5

--- CheckHotfixProject/pipelines/pip1.py
+++ CheckHotfixProject/pipelines/pip1.py
@@ -6,7 +6,7 @@ with DAG(Config = Config, Schedule = Schedule, SensorSchedule = SensorSchedule):
     email_hotfix_notification = Task(
         task_id = "email_hotfix_notification", 
         component = "Email", 
-        body = "test bb", 
+        body = "test bb2", 
         subject = "Hotfix pip1", 
         includeData = True, 
         fileName = "test.csv", 
