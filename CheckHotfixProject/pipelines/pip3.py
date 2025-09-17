cbca0aec1987186877e0b152a777265e4743c2bb
yuvraj@prophecy.io <yuvraj@prophecy.io>
2025-09-17 10:06:54 +0000

    v32

--- CheckHotfixProject/pipelines/pip3.py
+++ CheckHotfixProject/pipelines/pip3.py
@@ -1,4 +1,4 @@
-Schedule = Schedule(cron = "0 0/5 * * * ? *", timezone = "Asia/Kolkata")
+Schedule = Schedule(cron = "0 0/5 * * * ? *", timezone = "Asia/Kolkata", enabled = False)
 SensorSchedule = SensorSchedule(enabled = False)
 
 with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
